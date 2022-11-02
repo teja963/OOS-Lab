19e3cb05e3281cc18a41fa640c570637579de92d
teja963 <panasateja123@gmail.com>
2022-11-02 12:29:25 +0530

    Assg8 3rd que completed

--- Assg8/q2.java
+++ Assg8/q2.java
@@ -7,27 +7,27 @@ public class q2
     {
         try
 		{
-		    File f1=new File(filename);
-    		Scanner data=new Scanner(f1);
+		    File f1 = new File(filename);
+    		Scanner data = new Scanner(f1);
     		while(data.hasNextLine())
     		{
-    		    String line=data.nextLine();
-    		    int size=line.length();
-    		    int flag=0;
-    		    for(int i=0;i<size;i++)
+    		    String line = data.nextLine();
+    		    int size = line.length();
+    		    int flag = 0;
+    		    for(int i = 0; i < size; i++)
     		    {
-    		        if(line.charAt(i)>='0' && line.charAt(i)<='9') 
+    		        if(line.charAt(i) >= '0' && line.charAt(i) <= '9') 
     		        {
     		            continue;
     		        }
     		        else 
     		        {
-    		            flag=1;
+    		            flag = 1;
     		            System.out.println("OtherCharactersFound");
     		            break;
     		        }
     		    }
-    		    if(flag==0)
+    		    if(flag == 0)
     		    {
     		        System.out.println(line);
     		    }
@@ -42,8 +42,8 @@ public class q2
     }
 	public static void main(String[] args) 
 	{
-		Scanner sc=new Scanner(System.in);
-		String filename=sc.nextLine();
+		Scanner sc = new Scanner(System.in);
+		String filename = sc.nextLine();
 		printLine(filename);
 		
 	}
